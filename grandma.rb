b241ed31dc5c14097d6fe0b972a0020f07678c6b
Robert Leduc <rleduc34@gmail.com>
2020-05-07 21:32:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,6 @@
 def speak_to_grandma
 if "I love you Grandma!" == "I love grandma!" .uppercase
   puts "I LOVE YOU TOO PUMPKIN!"
-  binding.pry
-
 elsif "" == "" .uppercase
   puts "NO, NOT SINCE 1938!"
 else
