bbf19bc51c9566b0303054a67fc369ebd2e6a6f8
Robert Leduc <rleduc34@gmail.com>
2020-05-07 21:40:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,4 @@
-def speak_to_grandma
+
 if "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-elsif "WHAT DID YOU EAT TODAY?" == "what did you eat today?" .upcase
-  puts "NO, NOT SINCE 1938!"
-elsif "WHAT?" == "what?" .upcase
-  puts "NO, NOT SINCE 1938!"
-else
-  puts "HUH?! SPEAK UP, SONNY!"
-  end
 end
